c8750a100899ba9bcb646ef785b37e5f17ad25f3
Ideopedia <ideopedia00@gmail.com>
2022-11-07 13:44:51 +0530

    NOV&1344

--- pages/contact.js
+++ pages/contact.js
@@ -1,67 +1,75 @@
 import React from "react";
 import { useEffect, useRef } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import { useRouter } from "next/router";
+import Image from "next/image";
 import Navbar from "../components/navbar";
 import Link from "next/link";
 import contactfacebook from "../public/contactfacebook.svg";
 import contacttwitter from "../public/contacttwitter.svg";
 import contacttelegram from "../public/contacttelegram.svg";
 import map from "../public/map.svg";
-
 import mail from "../public/mail.svg";
 
 import location from "../public/location.svg";
-import Image from "next/image";
-function Contact() {
-  const email =
-    "https://mail.google.com/mail/u/0/?fs=1&to=info@ideopedia.org&su=&body=&tf=cm";
-  const googlemap = useRef(null);
 
+const New = () => {
   const FormHeader = (props) => <h2 id="headerTitle">{props.title}</h2>;
   return (
     <div>
       <Navbar />
-      <section class="p-4 h-screen new">
-        <div class="px-6 h-full text-gray-800">
-          <div class="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6 ">
-            <div class="grow-0 shrink-1 md:shrink-0 basis-auto py-8 xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0 new1 cim bg-green-700 rounded-md drop-shadow">
-              <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-2">
-                <div class="pt-4 pl-4 pr-4 rounded-md flex items-center justify-center">
-                  <Image src={map} />
-                </div>
-                <div
-                  class="rounded-md flex items-center justify-center  cursor-pointer"
-                  href={email}
+      <div className="p-4">
+        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8">
+          <div class=" rounded-md flex items-center justify-center bg-green-700 h-screen">
+            <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-2">
+              <div class="pt-4 pl-4 pr-4 rounded-md flex items-center justify-center">
+                <Link href="https://g.page/thinkindia?share">
+                  <a target="_blank">
+                    <Image src={map} />
+                  </a>
+                </Link>
+              </div>
+              <div class="rounded-md flex items-center justify-center cursor-pointer">
+                <span>
+                  <Image src={mail} />
+                </span>
+                <Link
+                  href="https://mail.google.com/mail/u/0/?fs=1&to=info@ideopedia.org&su=Regarding+Ideopedia&body=&tf=cm"
+                  passHref={true}
                 >
-                  <span>
-                    <Image src={mail} />
-                  </span>
-                  <span className="pl-2 pb-1 text-white text-xl ">
-                    info@ideopedia.org
-                  </span>
+                  <a target="_blank">
+                    <div className="pl-2 pb-1 text-white text-xl">
+                      info@ideopedia.org
+                    </div>
+                  </a>
+                </Link>
+              </div>
+              <div class="rounded-md flex items-center justify-center">
+                <span>
+                  <Image src={location} />
+                </span>
+                <Link href="https://g.page/thinkindia?share" passHref={true}>
+                  <a target="_blank">
+                    <div className="pl-2 pb-1 text-white text-xl">
+                      info@ideopedia.org
+                    </div>
+                  </a>
+                </Link>
+              </div>
+              <div class="pt-9 rounded-md flex items-end justify-center">
+                <div className="pl-3 ">
+                  <Image src={contactfacebook} />
                 </div>
-                <div class="rounded-md flex items-center justify-center">
-                  <span>
-                    <Image src={location} />
-                  </span>
-                  <span className="pl-2 pb-1 text-white text-xl">
-                    info@ideopedia.org
-                  </span>
+                <div className="pl-3 pb-1">
+                  <Image src={contacttwitter} />
                 </div>
-                <div class="pt-9 rounded-md flex items-end justify-center">
-                  <div className="pl-3 ">
-                    <Image src={contactfacebook} />
-                  </div>
-                  <div className="pl-3 pb-1">
-                    <Image src={contacttwitter} />
-                  </div>
-                  <div className="pl-3 pb-1">
-                    <Image src={contacttelegram} />
-                  </div>
+                <div className="pl-3 pb-1">
+                  <Image src={contacttelegram} />
                 </div>
               </div>
             </div>
-            <div class="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0 new">
+          </div>
+          <div class="rounded-md flex items-center justify-center pb-9">
+            <div>
               <FormHeader title="Contact us" className="bd1" />
               <div>
                 <form>
@@ -98,9 +106,9 @@ function Contact() {
             </div>
           </div>
         </div>
-      </section>
+      </div>
     </div>
   );
-}
+};
 
-export default Contact;
+export default New;
--- pages/new.js
+++ pages/new.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import SliderComponent from "../components/testimonialSlider"
-const New = () => {
-    return (
-        <div>
-            <SliderComponent/>
-        </div>
-    );
-}
+import React from "react";
+import GoogleMapReact from "google-map-react";
 
-export default New;
+const MapMarker = ({ text }) => <div>{text}</div>;
+
+export default function SimpleMap() {
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627,
+    },
+    zoom: 11,
+  };
+
+  return (
+    <div style={{ height: "100vh", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyAVg4h2Gp1ohkZxKqfh-T4KJGNRfsDKh0U" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <MapMarker text="Think India" />
+      </GoogleMapReact>
+    </div>
+  );
+}
