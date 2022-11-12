7ecd9ccc7c6eda499593ad33c96e4aeb4f77a537
ideopedia00@gmail.com <ideopedia00@gmail.com>
2022-11-12 13:26:02 +0530

    krishnan

---
+++ pages/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Newnavbar from '../components/newnavbar'
+
+const nav = () => {
+  return (
+    <div>
+        <Newnavbar />
+    </div>
+  )
+}
+
+export default nav
