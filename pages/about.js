0a30697d66c06c1d4f6c4ade8afeb4f363dddccf
ideopedia00@gmail.com <ideopedia00@gmail.com>
2022-11-16 20:06:11 +0530

    krish16nov

--- pages/about.js
+++ pages/about.js
@@ -157,8 +157,8 @@ const About = () => {
               </div>
           </div>
         </div>
-        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 pt-9 pb-9 keybgr">
-          <div class=" rounded-md flex items-center justify-center">
+        <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 lg:gap-8 pt-9 pb-9 bg-gray-50 keybgr">
+          <div class=" rounded-md flex items-center justify-center py-14">
             <Image src={aboutkeys} />
           </div>
           <div class="rounded-md flex items-center justify-center">
