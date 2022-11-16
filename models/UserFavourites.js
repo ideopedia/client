a5f8954116d37c5e8f63a445f08c96b72cb524ca
Ideopedia <ideopedia00@gmail.com>
2022-11-16 18:34:18 +0530

    jana161834

--- models/UserFavourites.js
+++ models/UserFavourites.js
@@ -2,13 +2,11 @@ import { Schema, model, models } from "mongoose";
 //import { Schema, model, models } from "mongoose";
 //datas and their data type which are stored our monoose database
 var userFavouritesSchema = new Schema({
-  Favourites: {
-    name: { type: String },
-    image: { type: String },
-    author: { type: String },
-    percent: { type: Number },
-    id: { type: Number },
-  },
+  name: { type: String },
+  image: { type: String },
+  author: { type: String },
+  percent: { type: Number },
+  id: { type: Number },
 
   User_Id: { type: String },
 });
