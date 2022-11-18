8ba03fd0e8953ea3bfa6361687ce795c9423221a
Ideopedia <ideopedia00@gmail.com>
2022-11-19 00:07:25 +0530

    jana190007

--- pages/data.js
+++ pages/data.js
@@ -1,24 +1,11 @@
-import * as React from "react";
-import { Range } from "react-range";
-import { useState } from "react";
-const RangeSlider = () => {
-  const [rangeval, setRangeval] = useState(null);
+import React from 'react';
 
+const Data = () => {
   return (
-    <div>
-      <input
-        class="rounded-lg overflow-hidden appearance-none bg-gray-400 h-3 w-[300px]"
-        type="range"
-        min="0"
-        max="10"
-        value={rangeval}
-        step="2"
-        onChange={(event) => setRangeval(event.target.value)}
-      />
-
-      {console.log(rangeval)}
+    <div className='h-[280px] w-[280px] bg-yellow-500'>
+      
     </div>
   );
-};
+}
 
-export default RangeSlider;
+export default Data;
